9c38393fa1e4e07a1cf25f14493812439062ab91
SiddiqurRahmnan88 <siddiqurr88@gmail.com>
2024-05-26 18:35:09 +0600

    Add files via upload
    Suppose the (n+1) values are (xi, yi), i = 0, 1, 2,..., n, where xi are not necessarily equidistant. In order to interpolate the values of y for given values of x of the tabulated data, Newton’s divided difference interpolation formula is used.

---
+++ Newton's Divided Difference Formula.R
@@ -0,0 +1,23 @@
+# Newton's Divided Difference Formula 
+NDDF<-function(x,y,x0){
+  sum<-0
+  dx<-1
+  del<-y
+  l<-length(x)-1
+  for(i in 1:l){
+    dx<-dx*(x0-x[i])
+    difx<-diff.default(x, lag=i, differences=1)
+    dify<-diff.default(del,lag=1, differences=1)
+    del<-dify/difx
+    sum<-sum+dx*del[1]
+  }
+  return(y[1]+sum)
+}
+x<-c(2.0, 2.2, 2.4, 2.7, 2.8)
+y<-c(0.9772499, 0.9860966, 0.9918025, 0.9965330, 0.9974449)
+NDDF(x, y, 2.1)	#interpolation
+# R output
+[1] 	0.9821355112
+NDDF(x, y, 2.75)	#extrapolation
+# R output
+[1] 	0.9970204143
